00da6c1906bfb7760973395c81d4996f1177365e
Prince <princevirmani410@gmail.com>
2024-12-13 23:01:56 +0530

    some changes

--- app.js
+++ app.js
@@ -1,4 +1,32 @@
 const express = require("express");
 const socket = require("socket.io");
 const http = require("http");
-const { chess } = require("chess.js");
+const { Chess } = require("chess.js");
+const path = require("path");
+const { log } = require("console");
+const { title } = require("process");
+
+const app = express();
+const server = http.createServer(app);
+
+const io = socket(server);
+
+const chess = new Chess();
+
+let players = {};
+let currentPlayer = "w";
+
+app.set("view engine", "ejs");
+app.use(express.static(path.join(__dirname, "public")));
+
+app.get("/", (req, res) => {
+  res.render("index", { title: "Chess Game" });
+});
+
+io.on("connection", (uniqueSocket) => {
+  console.log("Connected");
+});
+
+server.listen(3000, function () {
+  console.log("Listening on Port 3000");
+});
