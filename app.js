9ab438f71a95719dd215efcc553dd659f3c894af
Prince <princevirmani410@gmail.com>
2024-12-14 06:52:26 +0530

    backend functionaity

--- app.js
+++ app.js
@@ -23,8 +23,47 @@ app.get("/", (req, res) => {
   res.render("index", { title: "Chess Game" });
 });
 
-io.on("connection", (uniqueSocket) => {
+io.on("connection", function (uniqueSocket) {
   console.log("Connected");
+
+  if (!players.white) {
+    players.white = uniqueSocket.id;
+    uniqueSocket.emit("playerRole", "w");
+  } else if (!players.black) {
+    players.black = uniqueSocket.id;
+    uniqueSocket.emit("playerRole", "b");
+  } else {
+    uniqueSocket.emit("spectatorRole");
+  }
+});
+
+uniqueSocket.on("disconnect", function () {
+  if (uniqueSocket.id === players.white) {
+    delete players.white;
+  }
+  if (uniqueSocket.id === players.black) {
+    delete players.black;
+  }
+});
+
+uniqueSocket.on("move", (move) => {
+  try {
+    if (chess.turn() === "w" && uniqueSocket.id != white) return;
+    if (chess.turn() === "b" && uniqueSocket.id != black) return;
+
+    const result = chess.move(move);
+    if (result) {
+      currentPlayer = chess.turn();
+      io.emit("move", move);
+      io.emit("boardState", chess.fen());
+    } else {
+      console.log("Invalid move:", move);
+      uniqueSocket.emit("Invalid move:", move);
+    }
+  } catch (error) {
+    console.log(error);
+    uniqueSocket.emit("Invalid erorr", move);
+  }
 });
 
 server.listen(3000, function () {
